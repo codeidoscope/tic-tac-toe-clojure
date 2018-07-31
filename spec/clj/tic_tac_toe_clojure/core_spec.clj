67045de1532a2723ce1f105abad418ce65190e25
Marion LV <marion@8thlight.com>
2018-07-31 15:29:21 +0100

    Set up project and create board

---
+++ spec/clj/tic_tac_toe_clojure/core_spec.clj
@@ -0,0 +1,9 @@
+(ns tic-tac-toe-clojure.core-spec
+  (:require [speclj.core :refer :all]
+            [tic-tac-toe-clojure.core :refer :all]))
+
+(describe "A board"
+  (it "has 9 cells"
+    (should= 
+      [nil nil nil nil nil nil nil nil nil]
+      (create-cells))))
---
+++ src/clj/tic_tac_toe_clojure/core.clj
@@ -0,0 +1,4 @@
+(ns tic-tac-toe-clojure.core)
+
+(defn create-cells []
+  [nil nil nil nil nil nil nil nil nil])
