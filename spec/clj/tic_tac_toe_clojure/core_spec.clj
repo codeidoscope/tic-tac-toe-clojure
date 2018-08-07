dcc1d9d055fb2d5a5e1c56002ddcb517e9362687
Marion LV <marion@8thlight.com>
2018-08-07 18:04:52 +0100

    Create set-position

--- spec/clj/tic_tac_toe_clojure/core_spec.clj
+++ spec/clj/tic_tac_toe_clojure/core_spec.clj
@@ -1,17 +1,38 @@
-(ns tic-tac-toe-clojure.core-spec
-  (:require [speclj.core :refer :all]
-            [tic-tac-toe-clojure.core :refer :all]))
+(ns tic-tac-toe-clojure.core-spec (:require [speclj.core :refer :all] [tic-tac-toe-clojure.core :refer :all]))
 
 (describe "A board"
   (it "has 9 cells"
-    (should= 
+    (should=
       ["_" "_" "_" "_" "_" "_" "_" "_" "_"]
       create-board))
+
+  (it "sets a X mark at the correct position"
+    (should=
+      ["X" "_" "_" "_" "_" "_" "_" "_" "_"]
+      (set-position create-board 0 "X")))
+
+  (it "sets a X mark O mark at the correct position"
+    (should=
+      ["X" "O" "_" "_" "_" "_" "_" "_" "_"]
+      (set-position (set-position create-board 0 "X") 1 "O")))
+
+  (it "sets a O mark at the correct position"
+    (should=
+      ["O" "_" "_" "_" "_" "_" "_" "_" "_"]
+      (set-position create-board 0 "O")))
+
   (it "is displayed on three lines"
        (should=
+         "___\n___\n___"
+         (display-board create-board))
          "___\n___\n___\n"
-         (display-board create-board)))
-  (it "shows numbered cells"
       (should=
-        "123\n456\n789\n"
-        (display-board numbered-board))))
+        "123\n456\n789"
+        (display-board numbered-board)))
+  )
+
+(describe "A game"
+  (it "prompts a user to pick a symbol"
+      (should-contain
+        "Y"
+       (with-in-str "Y" (get-user-symbol)))))
--- src/clj/tic_tac_toe_clojure/core.clj
+++ src/clj/tic_tac_toe_clojure/core.clj
@@ -1,6 +1,4 @@
-(ns tic-tac-toe-clojure.core
-  (:require [clojure.string :as string]))
-
+(ns tic-tac-toe-clojure.core (:require [clojure.string :as string]))
 (defn -main [& args]
   (println "Working!"))
 
@@ -11,4 +9,10 @@
   ["1" "2" "3" "4" "5" "6" "7" "8" "9"])
 
 (defn display-board [board]
-  (with-out-str (println (string/join "\n" (re-seq #".{1,3}" (apply str board))))))
+  (with-out-str (print (string/join "\n" (re-seq #".{1,3}" (apply str board))))))
+
+(defn get-user-symbol []
+  (do (print "Please choose a symbol (X or O): ") (flush) (read-line)))
+
+(defn set-position [board position symbol]
+  (assoc board position symbol))
