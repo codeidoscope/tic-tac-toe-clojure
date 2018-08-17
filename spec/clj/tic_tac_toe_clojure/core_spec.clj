c038ed6761f99b64fb5f44079398385982cd6bf5
Marion LV <marion@8thlight.com>
2018-08-17 12:59:54 +0100

    Add instructions to README

--- spec/clj/tic_tac_toe_clojure/core_spec.clj
+++ spec/clj/tic_tac_toe_clojure/core_spec.clj
@@ -2,33 +2,32 @@
 
 (describe "A board"
   (it "has 9 cells"
-    (should=
-      ["_" "_" "_" "_" "_" "_" "_" "_" "_"]
-      create-board))
+      (should=
+        ["_" "_" "_" "_" "_" "_" "_" "_" "_"]
+        (create-board)))
 
   (it "sets a X mark at the correct position"
-    (should=
-      ["X" "_" "_" "_" "_" "_" "_" "_" "_"]
-      (set-position create-board 0 "X")))
+      (should=
+        ["X" "_" "_" "_" "_" "_" "_" "_" "_"]
+        (set-position (create-board) 0 "X")))
 
   (it "sets a X mark O mark at the correct position"
-    (should=
-      ["X" "O" "_" "_" "_" "_" "_" "_" "_"]
-      (set-position (set-position create-board 0 "X") 1 "O")))
+      (should=
+        ["X" "O" "_" "_" "_" "_" "_" "_" "_"]
+        (set-position (set-position (create-board) 0 "X") 1 "O")))
 
   (it "sets a O mark at the correct position"
-    (should=
-      ["O" "_" "_" "_" "_" "_" "_" "_" "_"]
-      (set-position create-board 0 "O")))
+      (should=
+        ["O" "_" "_" "_" "_" "_" "_" "_" "_"]
+        (set-position (create-board) 0 "O")))
 
   (it "is displayed on three lines"
-       (should=
-         "___\n___\n___"
-         (display-board create-board))
-         "___\n___\n___\n"
       (should=
-        "123\n456\n789"
-        (display-board numbered-board))))
+        "___\n___\n___\n"
+        (display-board (create-board)))
+      (should=
+        "123\n456\n789\n"
+        (display-board (numbered-board)))))
 
 (describe "A game"
   (it "prompts a user to pick a symbol"
@@ -48,49 +47,49 @@
 
   (it "tells the user the game is over"
       (should=
-        "Game is over"
+        "Game is over\n"
         (end-game))))
 
 (describe "A decision engine"
   (it "gets the rows from a board"
       (should=
         [["X" "_" "_"] ["O" "_" "_"] ["X" "_" "_"]]
-        (get-rows (set-position (set-position (set-position create-board 0 "X") 3 "O") 6 "X"))))
+        (get-rows (set-position (set-position (set-position (create-board) 0 "X") 3 "O") 6 "X"))))
 
   (it "gets the columns from a board"
       (should=
         [["X" "_" "_"] ["O" "_" "_"] ["X" "_" "_"]]
-        (get-columns (set-position (set-position (set-position create-board 0 "X") 1 "O") 2 "X"))))
+        (get-columns (set-position (set-position (set-position (create-board) 0 "X") 1 "O") 2 "X"))))
 
   (it "gets the diagonals from a board"
       (should=
         [["X" "O" "X"] ["O" "O" "O"]]
-        (get-diagonals (set-position (set-position (set-position (set-position (set-position create-board 0 "X") 4 "O") 8 "X") 2 "O") 6 "O"))))
+        (get-diagonals (set-position (set-position (set-position (set-position (set-position (create-board) 0 "X") 4 "O") 8 "X") 2 "O") 6 "O"))))
 
   (it "joins the rows, columns and diagonals"
      (should=
        [["X" "_" "O"] ["_" "X" "_"] ["O" "_" "X"] ["X" "_" "O"] ["_" "X" "_"] ["O" "_" "X"] ["X" "X" "X"] ["O" "X" "O"]]
-       (join-sections (set-position (set-position (set-position (set-position (set-position create-board 0 "X") 4 "X") 8 "X") 2 "O") 6 "O"))))
+       (join-sections (set-position (set-position (set-position (set-position (set-position (create-board) 0 "X") 4 "X") 8 "X") 2 "O") 6 "O"))))
 
   (it "checks if three symbols are aligned"
       (should=
         true
-        (three-mapped? (set-position (set-position (set-position (set-position (set-position create-board 0 "X") 4 "X") 8 "X") 2 "O") 6 "O"))))
+        (three-mapped? (set-position (set-position (set-position (set-position (set-position (create-board) 0 "X") 4 "X") 8 "X") 2 "O") 6 "O"))))
 
   (it "returns true if a board is full"
       (should=
         true
-        (board-full? (set-position (set-position (set-position (set-position (set-position (set-position (set-position (set-position (set-position create-board 0 "X") 1 "O") 2 "X") 3 "O") 4 "X") 5 "O") 6 "X") 7 "O") 8 "X"))))
+        (board-full? (set-position (set-position (set-position (set-position (set-position (set-position (set-position (set-position (set-position (create-board) 0 "X") 1 "O") 2 "X") 3 "O") 4 "X") 5 "O") 6 "X") 7 "O") 8 "X"))))
 
   (it "returns false if a board is not full"
       (should=
         false
-        (board-full? (set-position (set-position (set-position (set-position (set-position (set-position (set-position (set-position (set-position create-board 0 "X") 1 "O") 2 "X") 3 "O") 4 "X") 5 "O") 6 "X") 7 "O") 8 "_"))))
+        (board-full? (set-position (set-position (set-position (set-position (set-position (set-position (set-position (set-position (set-position (create-board) 0 "X") 1 "O") 2 "X") 3 "O") 4 "X") 5 "O") 6 "X") 7 "O") 8 "_"))))
 
   (it "returns true if the game is over because three symbols are aligned"
       (should=
         true
-        (game-over? (set-position (set-position (set-position (set-position (set-position create-board 0 "X") 4 "X") 8 "X") 2 "O") 6 "O"))))
+        (game-over? (set-position (set-position (set-position (set-position (set-position (create-board) 0 "X") 4 "X") 8 "X") 2 "O") 6 "O"))))
 
   (it "returns true if the game is over because the board is full"
       (should=
--- src/clj/tic_tac_toe_clojure/core.clj
+++ src/clj/tic_tac_toe_clojure/core.clj
@@ -1,27 +1,26 @@
 (ns tic-tac-toe-clojure.core (:require [clojure.string :as string]))
 (declare start-game)
 (defn -main [& args]
-  (if-not (first args)
-  (start-game)
-  (println "Hello" (first args))))
+  (println "Starting game")
+  (start-game))
 
-(def create-board
+(defn create-board []
   (into [] (repeat 9 "_")))
 
-(def numbered-board
+(defn numbered-board []
   ["1" "2" "3" "4" "5" "6" "7" "8" "9"])
 
 (defn display-board [board]
-  (with-out-str (print (string/join "\n" (re-seq #".{1,3}" (apply str board))))))
+  (with-out-str (println (string/join "\n" (re-seq #".{1,3}" (apply str board))))))
 
 (defn get-user-symbol []
   (do (print "Please choose a symbol (X or O): ") (flush) (read-line)))
 
 (defn get-user-position []
-   (do (print "Please choose a position between 1 and 9: ") (flush) (read-line)))
+  (apply str (do (print "Please choose a position between 1 and 9: ") (flush) (read-line))))
 
-(defn set-position [board position symbol]
-   (assoc board position symbol))
+(defn set-position [board position user-symbol]
+  (assoc board position user-symbol))
 
 (defn swap-player [user-symbol]
   (if (= "X" user-symbol) "O" "X"))
@@ -40,7 +39,7 @@
   (concat (get-rows board) (concat (get-columns board)) (concat(get-diagonals board))))
 
 (defn symbols-equal? [section]
-  (if (and (= (nth section 0) (nth section 1) (nth section 2)) (not= (str section) "___")) true false))
+  (if (and (= (nth section 0) (nth section 1) (nth section 2)) (not= (apply str section) "___")) true false))
 
 (defn three-aligned? [board]
   (some true? (for [section (join-sections board)] (symbols-equal? section))))
@@ -52,19 +51,21 @@
   (if (some #{"_"} board) false true))
 
 (defn end-game []
-  (with-out-str (print "Game is over")))
+  (with-out-str (println "Game is over")))
 
 (defn game-over? [board]
   (if (or (three-aligned? board) (board-full? board)) true false))
 
 (defn next-player-turn [board user-symbol]
-   (set-position board (get-user-position) user-symbol)
-   (swap-player user-symbol))
+ (display-board board)
+ (set-position board (get-user-position) user-symbol)
+ (swap-player user-symbol))
 
 (defn take-turn [board user-symbol]
-  (if (game-over? board) (end-game) (next-player-turn board user-symbol)))
+  (while (= (game-over? board) false) (do (next-player-turn board user-symbol))))
 
 (defn start-game []
   (let [user-symbol (get-user-symbol)]
-  (display-board numbered-board)
-  (take-turn (create-board) user-symbol)))
+    (display-board (numbered-board))
+    (let [board (create-board)]
+      (if (game-over? (next-player-turn board user-symbol)) (end-game) (next-player-turn board user-symbol)))))
